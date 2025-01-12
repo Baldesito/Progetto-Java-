cdf38065c4204c874f72de34465bc651405ebe99
OCCHIO NERO <112170802+Baldesito@users.noreply.github.com>
2025-01-12 15:45:07 +0100

    astratta

---
+++ src/Media.java
@@ -0,0 +1,10 @@
+public abstract class Media {
+    protected String titolo;
+
+    public Media(String titolo){
+        this.titolo = titolo;
+    }
+    public String getTitolo() {
+        return titolo;
+    }
+}
